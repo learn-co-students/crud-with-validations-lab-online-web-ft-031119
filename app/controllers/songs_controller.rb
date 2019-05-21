1a6421edcd7a64654f657c7a326ce5f567c931c1
Jon <jhharwood88@gmail.com>
2019-05-21 19:38:43 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,7 @@ class SongsController < ApplicationController
 
 	def create
 		@song = Song.new(song_params)
-
+		
 		if @song.valid? 
 			@song.save
 			redirect_to song_path(@song)
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,17 @@
 class Song < ApplicationRecord
 
 	validates :title, :artist_name, presence: true
-	validates :release_year, presence: true, if: :released?
+	# validates :released, inclusion: { in: [true, false]}
+
+	#  with_options if: :released? do |song|
+	#     song.validates :release_year, presence: true
+	# end
+
+	
  
-	  def released?
-	    released == true
-	  end
+	#   def released?
+	#     released 
+	#   end
 	
 
 end
