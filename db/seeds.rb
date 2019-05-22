d91c62c956829203356c320d29642d0f76ed1adf
Annie Phan <anniephan.atp@gmail.com>
2019-05-22 13:10:09 -0700

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Song.create(title: "Talisman", artist_name: "Air", release_year: 2007, released: true, genre: "Post-Rock")
+
+Song.create(title: "Nightmare", artist_name: "Halsey", release_year: 2019, released: true, genre: "Pop-Punk")
+
+Song.create(title: "Lose Yourself", artist_name: "Eminem", release_year: 2002, released: true, genre: "Rap")
